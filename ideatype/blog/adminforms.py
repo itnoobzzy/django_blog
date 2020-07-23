e89878bd0c285a9ef88034d40b714ead9b7f846b
zzy <zzy@zzy.com>
2020-07-23 18:31:51 +0800

    自定义后台管理form样式

---
+++ ideatype/blog/adminforms.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2020/7/23 15:19
+# @Author : zhouzy_a
+# @Version：V 0.1
+# @File : adminforms.py
+# @desc :自定义后台管理Form
+
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+    desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
+
+
+
+
+if __name__ == '__main__':
+    pass
