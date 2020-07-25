a5c484445772ad93d1008f0c76d10479252a6a03
zzy <zzy@zzy.com>
2020-07-25 14:39:20 +0800

    model层增加类方法，静态方法和属性方法来供view层获取数据

--- ideatype/blog/models.py
+++ ideatype/blog/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
 
 class Category(models.Model):
     STATUS_NORMAL = 1
@@ -24,6 +26,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def get_navs(cls):
+        categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories,
+        }
+
+
 class Tag(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
@@ -34,7 +53,7 @@ class Tag(models.Model):
 
     name = models.CharField(max_length=10, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
-        choices=STATUS_ITEMS, verbose_name="状态")
+                                         choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
@@ -43,6 +62,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Post(models.Model):
     STATUS_NORMAL = 1
@@ -58,12 +78,50 @@ class Post(models.Model):
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为Markdown格式")
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
-        choices=STATUS_ITEMS, verbose_name="状态")
+                                         choices=STATUS_ITEMS, verbose_name="状态")
     is_md = models.BooleanField(default=False, verbose_name="markdown语法")
-    category = models.ForeignKey(Category, default=STATUS_NORMAL,verbose_name="分类", on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, default=STATUS_NORMAL, verbose_name="分类", on_delete=models.DO_NOTHING)
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者", null=True)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_lists = []
+        else:
+            post_lists = tag.post_set.filter(status=Post.STATUS_NORMAL) \
+                .select_related('owner', 'category')
+
+        return post_lists, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_lists = []
+        else:
+            post_lists = category.post_set.filter(status=Post.STATUS_NORMAL)\
+                .select_related('owner', 'category')
+
+        return post_lists, category
+
+    @classmethod
+    def latest_posts(cls):
+        """"""
+        return cls.objects.filter(status=cls.STATUS_NORMAL)
+
+    @classmethod
+    def hot_posts(cls):
+        """返回降序排序的热门文章"""
+        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
